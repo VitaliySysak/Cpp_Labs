e541d7d032bc1366fd06b985926b3634a156691b
Vitaliy <bering856@gmail.com>
2024-09-16 19:47:28 +0300

    lab1 and lab2

---
+++ Lab1/main.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main() {
+    double a = 2, b = 3, c = 7;
+    double hx = 0.31;
+    
+    for (double x = 0; x <= 5; x += hx) {
+        double y;
+        
+        if (x < 2) {
+            y = pow(a, (b - x)) + c;
+        } 
+        else if (x >= 2 && x <= 4) {
+            y = pow(b, (c - x)) + a;
+        } 
+        else {
+            y = pow(c, (a - x)) + b;
+        }
+        cout.precision(12);
+        cout << "x = " << x << "\t y = " << y << endl;
+    }
+    
+    return 0;
+}
---
+++ Lab2/main.cpp
@@ -0,0 +1,48 @@
+
+
+#include <iostream>
+#include <cstdlib> 
+#include <ctime>    
+
+const int N = 5;  
+
+int main() {
+    int matrix[N][N];
+    int sumEven = 0;
+
+    srand(time(0));
+    std::cout << "Original Matrix:\n";
+    for (int i = 0; i < N; ++i) {
+        for (int j = 0; j < N; ++j) {
+            matrix[i][j] = rand() % 20;
+            std::cout << matrix[i][j] << "\t";
+        }
+        std::cout << std::endl;
+    }
+
+    for (int i = 0; i < N; ++i) {
+        for (int j = i + 1; j < N; ++j) {
+            if (matrix[i][j] % 2 == 0) {
+                sumEven += matrix[i][j];
+            }
+        }
+    }
+
+    std::cout << "\nSum of even elements above the main diagonal: " << sumEven << std::endl;
+
+    if (sumEven % 3 == 0) {
+        for (int i = 0; i < N; ++i) {
+            matrix[i][N - 1 - i] = 0;
+        }
+
+        for (int i = 0; i < N; ++i) {
+            for (int j = 0; j < N; ++j) {
+                std::cout << matrix[i][j] << "\t";
+            }
+            std::cout << std::endl;
+        }
+    }
+
+    return 0;
+}
+
